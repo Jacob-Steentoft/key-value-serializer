542fcb80370d619fb572f9d814e80b687673b0ed
Jacob Steentoft <jacob@steentoft.net>
2023-03-27 17:58:47 +0200

    updated benchmark to read and write from a memory stream

--- benchmarks/Benchmarks/CacheSerializeBenchmark.cs
+++ benchmarks/Benchmarks/CacheSerializeBenchmark.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using BenchmarkDotNet.Attributes;
 using KeyValueSerializer.Benchmarks.Models;
 using KeyValueSerializer.Cache;
@@ -9,37 +10,122 @@ namespace KeyValueSerializer.Benchmarks.Benchmarks;
 [MemoryDiagnoser]
 public class CacheSerializeBenchmark
 {
-    private const string FileFolder = "SupportFiles";
-    private const string CurrentConfigFilePath = $"{FileFolder}/arma3server.server.cfg";
-    private const string NewConfigFilePath = $"{FileFolder}/arma3server.server.new.cfg";
-    private readonly KeyValueCache _cache = new(typeof(ArmaServerOptions));
+    private readonly KeyValueCache _cache = new(typeof(TestSerial));
     private readonly KeyValueConfiguration _configuration = new();
     private readonly CancellationToken _cancellationToken = new();
-    
-    private readonly ArmaServerOptions _armaServerOptions = new()
+    private readonly TestSerial _testSerial = new()
     {
-        KickDuplicate = 0,
-        Admins = new []{ "dave", "chuck"},
-        Hostname = "Håst",
-        ForcedDifficulty = "test",
-        DisconnectTimeout = 2,
-        BattlEye = 0,
-        VoteThreshold = 10.33,
-        Upnp = true
+        String = "TestString",
+        Strings = new[] { "One", "Two", "Th\\\"ree" },
+        Bool = true,
+        Bools = new[] { true, false, true },
+        DateTime = new DateTime(2023, 1, 1),
+        DateTimes = new[]
+        {
+            new DateTime(2023, 1, 2),
+            new DateTime(2022, 1, 2),
+            new DateTime(2021, 1, 2)
+        },
+        DateTimeOffset = new DateTimeOffset(new DateTime(2023, 1, 3), TimeSpan.Zero),
+        DateTimeOffsets = new[]
+        {
+            new DateTimeOffset(new DateTime(2023, 1, 4), TimeSpan.Zero),
+            new DateTimeOffset(new DateTime(2023, 1, 5), TimeSpan.Zero)
+        },
+        TimeSpan = TimeSpan.FromHours(1),
+        TimeSpans = new[] { TimeSpan.FromHours(2), TimeSpan.FromHours(3) },
+        Guid = Guid.Parse("12345678-abcd-1234-abcd-1234567890ab"),
+        Guids = new[]
+        {
+            Guid.Parse("22345678-abcd-1234-abcd-1234567890ab"),
+            Guid.Parse("32345678-abcd-1234-abcd-1234567890ab")
+        },
+        Sbyte = 1,
+        Sbytes = new sbyte[] { 2, 3, 4 },
+        Byte = 5,
+        Bytes = new byte[] { 6, 7, 8 },
+        Short = 9,
+        Shorts = new short[] { 10, 11, 12 },
+        Ushort = 13,
+        Ushorts = new ushort[] { 14, 15, 16 },
+        Int = 17,
+        Ints = new[] { 18, 19, 20 },
+        Uint = 21U,
+        Units = new[] { 22U, 23U, 24U },
+        Long = 25L,
+        Longs = new[] { 26L, 27L, 28L },
+        Ulong = 29UL,
+        Ulongs = new[] { 30UL, 31UL, 32UL },
+        Float = 33.3f,
+        Floats = new[] { 34.4f, 35.5f, 36.6f },
+        Double = 37.7,
+        Doubles = new[] { 38.8, 39.9, 40.0 },
+        Decimal = 41.1m,
+        Decimals = new[] { 42.2m, 43.3m, 44.4m }
     };
+    private readonly byte[] _testSerialBytes;
+
+    public CacheSerializeBenchmark()
+    {
+        var newLine = (char)_configuration.ValueEnd + Encoding.UTF8.GetString(_configuration.NewLine);
+        var keyValueSeparator = Encoding.UTF8.GetString(new[] { _configuration.Space, _configuration.ValueStart, _configuration.Space });
+        var stringSeparator = (char)_configuration.StringSeparator;
+        var arrayStart = (char)_configuration.ArrayStart;
+        var arraySeparator = Encoding.UTF8.GetString(new[] { _configuration.ArraySeparator, _configuration.Space });
+        var arrayEnd = (char)_configuration.ArrayEnd;
+        var stringEscape = Encoding.UTF8.GetString(new[] { _configuration.StringIgnoreCharacter, _configuration.StringSeparator });
+        
+        var deserialTestSerial = 
+            $"string{keyValueSeparator}{stringSeparator}TestString{stringSeparator}{newLine}" +
+            $"strings{keyValueSeparator}{arrayStart}{stringSeparator}One{stringSeparator}{arraySeparator}{stringSeparator}Two{stringSeparator}{arraySeparator}{stringSeparator}Th{stringEscape}ree{stringSeparator}{arrayEnd}{newLine}" +
+            $"bool{keyValueSeparator}True{newLine}" +
+            $"bools{keyValueSeparator}{arrayStart}True{arraySeparator}False{arraySeparator}True{arrayEnd}{newLine}" +
+            $"dateTime{keyValueSeparator}2023-01-01T00:00:00.0000000{newLine}" +
+            $"dateTimes{keyValueSeparator}{arrayStart}2023-01-02T00:00:00.0000000{arraySeparator}2022-01-02T00:00:00.0000000{arraySeparator}2021-01-02T00:00:00.0000000{arrayEnd}{newLine}" +
+            $"dateTimeOffset{keyValueSeparator}2023-01-03T00:00:00.0000000+00:00{newLine}" +
+            $"dateTimeOffsets{keyValueSeparator}{arrayStart}2023-01-04T00:00:00.0000000+00:00{arraySeparator}2023-01-05T00:00:00.0000000+00:00{arrayEnd}{newLine}" +
+            $"timeSpan{keyValueSeparator}01:00:00{newLine}" +
+            $"timeSpans{keyValueSeparator}{arrayStart}02:00:00{arraySeparator}03:00:00{arrayEnd}{newLine}" +
+            $"guid{keyValueSeparator}12345678-abcd-1234-abcd-1234567890ab{newLine}" +
+            $"guids{keyValueSeparator}{arrayStart}22345678-abcd-1234-abcd-1234567890ab{arraySeparator}32345678-abcd-1234-abcd-1234567890ab{arrayEnd}{newLine}" +
+            $"sbyte{keyValueSeparator}1{newLine}" +
+            $"sbytes{keyValueSeparator}{arrayStart}2{arraySeparator}3{arraySeparator}4{arrayEnd}{newLine}" +
+            $"byte{keyValueSeparator}5{newLine}" +
+            $"bytes{keyValueSeparator}{arrayStart}6{arraySeparator}7{arraySeparator}8{arrayEnd}{newLine}" +
+            $"short{keyValueSeparator}9{newLine}" +
+            $"shorts{keyValueSeparator}{arrayStart}10{arraySeparator}11{arraySeparator}12{arrayEnd}{newLine}" +
+            $"ushort{keyValueSeparator}13{newLine}" +
+            $"ushorts{keyValueSeparator}{arrayStart}14{arraySeparator}15{arraySeparator}16{arrayEnd}{newLine}" +
+            $"int{keyValueSeparator}17{newLine}" +
+            $"ints{keyValueSeparator}{arrayStart}18{arraySeparator}19{arraySeparator}20{arrayEnd}{newLine}" +
+            $"uint{keyValueSeparator}21{newLine}" +
+            $"uints{keyValueSeparator}{arrayStart}22{arraySeparator}23{arraySeparator}24{arrayEnd}{newLine}" +
+            $"long{keyValueSeparator}25{newLine}" +
+            $"longs{keyValueSeparator}{arrayStart}26{arraySeparator}27{arraySeparator}28{arrayEnd}{newLine}" +
+            $"ulong{keyValueSeparator}29{newLine}" +
+            $"ulongs{keyValueSeparator}{arrayStart}30{arraySeparator}31{arraySeparator}32{arrayEnd}{newLine}" +
+            $"float{keyValueSeparator}33.3{newLine}" +
+            $"floats{keyValueSeparator}{arrayStart}34.4{arraySeparator}35.5{arraySeparator}36.6{arrayEnd}{newLine}" +
+            $"double{keyValueSeparator}37.7{newLine}" +
+            $"doubles{keyValueSeparator}{arrayStart}38.8{arraySeparator}39.9{arraySeparator}40{arrayEnd}{newLine}" +
+            $"decimal{keyValueSeparator}41.1{newLine}" +
+            $"decimals{keyValueSeparator}{arrayStart}42.2{arraySeparator}43.3{arraySeparator}44.4{arrayEnd}{newLine}";
+
+        _testSerialBytes = Encoding.UTF8.GetBytes(deserialTestSerial);
+    }
 
     [Benchmark]
-    public async ValueTask<ArmaServerOptions> CacheDeserializeAsync()
+    public async ValueTask<TestSerial> CacheDeserializeAsync()
     {
-        await using var fileStream = File.OpenRead(CurrentConfigFilePath);
-        return await Deserializer.DeserializeStreamAsync<ArmaServerOptions>(fileStream, _cache, _configuration, _cancellationToken);
+        using var memoryStream = new MemoryStream(_testSerialBytes);
+        return await Deserializer.DeserializeStreamAsync<TestSerial>(memoryStream, _cache, _configuration,
+            _cancellationToken);
     }
 
     [Benchmark]
     public void CacheSerialize()
     {
-        using var fileStream = File.OpenWrite(NewConfigFilePath);
-        Serializer.Serialize(_armaServerOptions, fileStream, _cache, _configuration);
+        using var memoryStream = new MemoryStream();
+        Serializer.Serialize(_testSerial, memoryStream, _cache, _configuration);
     }
-
 }
---
+++ benchmarks/Models/TestSerial.cs
@@ -0,0 +1,106 @@
+﻿namespace KeyValueSerializer.Benchmarks.Models;
+
+public class TestSerial
+{
+	[KeyFileName("string")]
+	public string? String { get; set; }
+
+    [KeyFileName("strings")]
+    public string[]? Strings { get; set; }
+
+    [KeyFileName("bool")]
+    public bool? Bool { get; set; }
+
+    [KeyFileName("bools")]
+    public bool[]? Bools { get; set; }
+    
+    [KeyFileName("dateTime")]
+    public DateTime? DateTime { get; set; }
+    
+    [KeyFileName("dateTimes")]
+    public DateTime[]? DateTimes { get; set; }
+    
+    [KeyFileName("dateTimeOffset")]
+    public DateTimeOffset? DateTimeOffset { get; set; }
+    
+    [KeyFileName("dateTimeOffsets")]
+    public DateTimeOffset[]? DateTimeOffsets { get; set; }
+    
+    [KeyFileName("timeSpan")]
+    public TimeSpan? TimeSpan { get; set; }
+    
+    [KeyFileName("timeSpans")]
+    public TimeSpan[]? TimeSpans { get; set; }
+    
+    [KeyFileName("guid")]
+    public Guid? Guid { get; set; }
+    
+    [KeyFileName("guids")]
+    public Guid[]? Guids { get; set; }
+    
+    [KeyFileName("sbyte")]
+    public sbyte? Sbyte { get; set; }
+    
+    [KeyFileName("sbytes")]
+    public sbyte[]? Sbytes { get; set; }
+    
+    [KeyFileName("byte")]
+    public byte? Byte { get; set; }
+    
+    [KeyFileName("bytes")]
+    public byte[]? Bytes { get; set; }
+    
+    [KeyFileName("short")]
+    public short? Short { get; set; }
+    
+    [KeyFileName("shorts")]
+    public short[]? Shorts { get; set; }
+    
+    [KeyFileName("ushort")]
+    public ushort? Ushort { get; set; }
+    
+    [KeyFileName("ushorts")]
+    public ushort[]? Ushorts { get; set; }
+    
+    [KeyFileName("int")]
+    public int? Int { get; set; }
+    
+    [KeyFileName("ints")]
+    public int[]? Ints { get; set; }
+
+    [KeyFileName("uint")]
+	public uint? Uint { get; set; }
+
+    [KeyFileName("uints")]
+    public uint[]? Units { get; set; }
+    
+    [KeyFileName("long")]
+    public long? Long { get; set; }
+
+    [KeyFileName("longs")]
+    public long[]? Longs { get; set; }
+    
+    [KeyFileName("ulong")]
+    public ulong? Ulong { get; set; }
+
+    [KeyFileName("ulongs")]
+    public ulong[]? Ulongs { get; set; }
+    
+    [KeyFileName("float")]
+    public float? Float { get; set; }
+
+    [KeyFileName("floats")]
+    public float[]? Floats { get; set; }
+
+    [KeyFileName("double")]
+    public double? Double { get; set; }
+
+    [KeyFileName("doubles")]
+    public double[]? Doubles { get; set; }
+    
+    [KeyFileName("decimal")]
+    public decimal? Decimal { get; set; }
+
+    [KeyFileName("decimals")]
+    public decimal[]? Decimals { get; set; }
+}
